846d5027f3bc65a9641d1d8666d572f6394aeea9
yongsoung <dydrkfl223@gmail.com>
2024-07-17 19:32:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 218612 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,3 +1,11 @@
-c = int(input())
-n = list(map(int, input().split()))
-print(min(n),max(n))
+n = int(input())
+a = list(map(int, input().split()))
+a_min = 10**9
+a_max = -10**9
+
+for i in range(n):
+    if a[i] < a_min:
+        a_min = a[i]
+    if a[i] > a_max:
+        a_max = a[i]
+print(a_min, a_max)
