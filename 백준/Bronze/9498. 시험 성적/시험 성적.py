401b9e8871e652bd1e4a5833a94845b8cdb410b2
김용성 <dydrkfl223@gmail.com>
2024-10-08 09:39:12 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,14 @@
-a = int(input())
-if a >= 90:
-    print('A')
-elif 80 <= a <= 89:
-    print('B')
-elif 70 <= a <= 79:
-    print('C')
-elif 60 <= a <= 69:
-    print('D')
+a = input()
+a = int(a)
+
+
+if(90 <= a <= 100):
+    print('A');
+elif(80 <= a <= 89):
+    print('B');
+elif(70 <= a <= 79):
+    print('C');
+elif(60 <= a <= 69):
+    print('D');
 else:
     print('F')
